8af3f5d9ce472975d22c32233e57da7c5f5c57b1
varun290101 <73779567+varun290101@users.noreply.github.com>
2021-10-01 23:57:45 +0530

    Create 296.py

---
+++ PYTHON/296.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+#stack
+stack = []
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print('Initial stack')
+print(stack)
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print('\nStack after elements are popped:')
+print(stack)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
