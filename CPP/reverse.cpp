71c273368b2b147524a29d1965d6b72597218219
mohit <mvk1407@gmail.com>
2021-10-01 20:16:12 +0530

    first

---
+++ CPP/reverse.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void reverse(int arr[], int n)
+{
+    reverse(arr, arr + n);
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    reverse(arr, n);
+    print(arr, n);
+
+    return 0;
+}
