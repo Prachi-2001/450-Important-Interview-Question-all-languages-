c3cae0ac854d1e9d5a655c6f984d875ceb61326f
Alisha <achauhan2_be20@thapar.edu>
2021-10-02 18:24:33 +0530

    Chocolate Distribution Problem Solved

---
+++ CPP/Chocolate_Distribution_problem.cpp
@@ -0,0 +1,51 @@
+//Chocolate Distribution problem - question number 35-Array
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    sort(a.begin(),a.end());
+        long long i = 0, j = 0,difference;
+        difference = INT_MAX;
+        while(j < n)
+        {
+            if(j-i+1 < m)
+                j++;
+            else if(j-i+1 == m)
+            {
+                difference = min(difference, a[j]-a[i]);
+                i++;
+                j++;
+            }
+        }
+        return difference;
+}
+};
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
